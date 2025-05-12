975defc939e45612e532ef7766671504bc41c1f0
ketan_kokate <ketan_kokate@epam.com>
2025-05-12 19:55:21 +0530

    change

--- models.py
+++ models.py
@@ -7,13 +7,6 @@ from sqlalchemy.engine import Engine
 import sqlite3
 from uuid import uuid4
 
-# @event.listens_for(Engine, "connect")
-# def set_sqlite_pragma(dbapi_connection, connection_record):
-#     if isinstance(dbapi_connection, sqlite3.Connection):
-#         cursor = dbapi_connection.cursor()
-#         cursor.execute("PRAGMA journal_mode=WAL;")
-#         cursor.close()
-
 db = SQLAlchemy(session_options={"expire_on_commit": True})
 
 class User(db.Model):
